2bd8513b2d9f52d979db117ecefb309a3c12eb39
Jessica Susilo <jxsusilo@gmail.com>
2022-10-26 16:26:55 -0700

    Updated extract_next_links

--- scraper.py
+++ scraper.py
@@ -15,7 +15,11 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    next_links = list()
+    if resp.status == 200:
+        # TODO: get hyperlinks
+        pass
+    return next_links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
